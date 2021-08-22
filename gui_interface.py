9a3515e30a6f1dfddc9b7a612d33f127bc7d4404
dimitarhristov86 <dimitar_hristov86@abv.bg>
2021-08-22 23:04:45 +0300

    Car_parts_shop_gui

--- gui_interface.py
+++ gui_interface.py
@@ -1,13 +1,18 @@
 import sys
+
+import mysql.connector
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtGui as qtg
 from PyQt5 import QtCore as qtc
+import mysql.connector as mc
+from mysql.connector import connection
 
 
 class MainWidget(qtw.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setup_ui()
+        self.btn_login.clicked.connect(self.login)
         self.show()
 
     def setup_ui(self):
@@ -16,9 +21,9 @@ class MainWidget(qtw.QWidget):
         window = qtw.QLabel(self)
         window.setStyleSheet("background-image: url(/home/dimitar/PycharmProjects/Car_parts_shop_gui/images/image.png)")
         window.setFixedSize(960, 640)
-        login_input = qtw.QLineEdit(self)
-        password_input = qtw.QLineEdit(self)
-        password_input.setEchoMode(qtw.QLineEdit.Password)
+        self.login_input = qtw.QLineEdit(self)
+        self.password_input = qtw.QLineEdit(self)
+        self.password_input.setEchoMode(qtw.QLineEdit.Password)
         self.btn_login = qtw.QPushButton("Login")
         self.btn_exit = qtw.QPushButton("Exit")
         self.btn_registration = qtw.QPushButton("Registration")
@@ -28,10 +33,39 @@ class MainWidget(qtw.QWidget):
         buttons_layout.addWidget(self.btn_exit)
         buttons_layout.setSpacing(100)
         form_layout = qtw.QFormLayout()
-        form_layout.addRow("Enter email: ", login_input)
-        form_layout.addRow("Enter password: ", password_input)
+        form_layout.addRow("Enter email: ", self.login_input)
+        form_layout.addRow("Enter password: ", self.password_input)
         form_layout.addRow("", buttons_layout)
         self.setLayout(form_layout)
+
+    def login(self):
+        try:
+            email = self.login_input.text()
+            password = self.password_input.text()
+
+            mydb = mc.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="car_parts_gui"
+
+            )
+
+            mycursor = mydb.cursor()
+            query = f""" SELECT * FROM users WHERE 
+            email='{email}' AND password='{password}'
+            """
+            mycursor.execute(query)
+            result = mycursor.fetchall()
+
+            if result == None:
+                print("Incorrect email or password")
+
+            else:
+                print("You are logged in")
+
+        except mysql.connector.Error as e:
+            print(e)
 
 
 app = qtw.QApplication(sys.argv)
