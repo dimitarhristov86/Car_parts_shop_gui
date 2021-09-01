01d121f63d50338f0648ddf271bb18fbfb78821d
dimitarhristov86 <dimitar_hristov86@abv.bg>
2021-09-01 22:55:28 +0300

    Car_parts_shop_gui

--- gui_interface.py
+++ gui_interface.py
@@ -10,6 +10,9 @@ class MainWidget(qtw.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setup_ui()
+        self.textfield = qtw.QLabel()
+        self.textfield.setStyleSheet('color: red')
+        self.textfield.setText('')
         self.btn_sign_in.clicked.connect(self.sign_in)
         self.btn_sign_up.clicked.connect(self.sign_up_form)
         self.btn_exit.clicked.connect(exit)
@@ -18,21 +21,19 @@ class MainWidget(qtw.QWidget):
     def setup_ui(self):
         self.setWindowTitle("Car Parts Shop")
         self.setFixedSize(900, 422)
-        window = qtw.QLabel(self)
-        window.setStyleSheet("background-image: url(/home/dimitar/PycharmProjects/Car_parts_shop_gui/images/1.jpg)")
-        window.setFixedSize(900, 422)
+        self.window = qtw.QLabel(self)
+        self.window.setStyleSheet(
+            "background-image: url(/home/dimitar/PycharmProjects/Car_parts_shop_gui/images/1.jpg)")
+        self.window.setFixedSize(900, 422)
         self.btn_sign_in = qtw.QPushButton("Sign in")
         self.btn_exit = qtw.QPushButton("Exit")
         self.btn_sign_up = qtw.QPushButton("Sign up")
-        self.textfield = qtw.QLabel(self)
-        self.textfield.setStyleSheet('color: red')
         buttons_layout = qtw.QHBoxLayout()
         buttons_layout.addWidget(self.btn_sign_in)
         buttons_layout.addWidget(self.btn_sign_up)
         buttons_layout.addWidget(self.btn_exit)
         buttons_layout.setSpacing(100)
         form_layout = qtw.QFormLayout()
-        form_layout.addRow(self.textfield)
         form_layout.addRow("", buttons_layout)
         self.setLayout(form_layout)
 
@@ -42,13 +43,11 @@ class MainWidget(qtw.QWidget):
         self.login_input = qtw.QLineEdit(self)
         self.password_input = qtw.QLineEdit(self)
         self.password_input.setEchoMode(qtw.QLineEdit.Password)
-        self.textfield = qtw.QLabel(self)
-        self.textfield.setStyleSheet('color: red')
-        btn_submit = qtw.QPushButton('Submit')
-        btn_exit = qtw.QPushButton('Exit')
+        self.btn_submit = qtw.QPushButton('Submit')
+        self.btn_exit = qtw.QPushButton('Exit')
         btn_layout = qtw.QHBoxLayout()
-        btn_layout.addWidget(btn_submit)
-        btn_layout.addWidget(btn_exit)
+        btn_layout.addWidget(self.btn_submit)
+        btn_layout.addWidget(self.btn_exit)
         btn_layout.setSpacing(100)
         form_layout = qtw.QFormLayout()
         form_layout.addRow("Enter email: ", self.login_input)
@@ -57,9 +56,10 @@ class MainWidget(qtw.QWidget):
         form_layout.addRow("", btn_layout)
         self.setLayout(form_layout)
         self.show()
-        btn_submit.clicked.connect(self.user_verification)
-        self.main_menu()
-        btn_exit.clicked.connect(exit)
+        self.btn_submit.clicked.connect(self.user_verification)
+        self.btn_exit.clicked.connect(exit)
+        if self.login_input.text() == 0 and self.password_input.text() == 0:
+            self.textfield.setText('Please fill all fields! ')
 
     def user_verification(self):
         try:
@@ -78,11 +78,12 @@ class MainWidget(qtw.QWidget):
             email='{email}' AND password='{password}'
             """
             mycursor.execute(query)
-            result = mycursor.fetchone()
-            if result == None:
+            self.result = mycursor.fetchone()
+            if self.result == None:
                 print("Incorrect email or password")
             else:
                 print("You are logged in")
+                self.main_menu()
         except mysql.connector.Error as e:
             print(e)
 
@@ -90,14 +91,14 @@ class MainWidget(qtw.QWidget):
         super().__init__()
         self.setWindowTitle('Sign up Form')
         self.setFixedSize(900, 422)
+        self.role = "user"
         self.user_first_name = qtw.QLineEdit(self)
         self.user_last_name = qtw.QLineEdit(self)
         self.user_email = qtw.QLineEdit(self)
         self.user_phone_number = qtw.QLineEdit(self)
         self.user_password = qtw.QLineEdit(self)
         self.user_confirm_password = qtw.QLineEdit(self)
-        date_time = datetime.now()
-        self.user_created = date_time.strftime("%Y/%m/%d %H:%M:%S")
+        self.user_created = datetime.now()
         self.btn_submit = qtw.QPushButton('Submit')
         self.btn_submit.setFixedSize(150, 50)
         self.textfield = qtw.QLabel(self)
@@ -118,42 +119,47 @@ class MainWidget(qtw.QWidget):
         self.setLayout(new_layout)
         self.show()
         self.btn_submit.clicked.connect(self.add_data_to_db)
-        user_input = [self.user_first_name, self.user_last_name, self.user_email, self.user_phone_number,
-                      self.user_password, self.user_confirm_password]
-        for item in user_input:
-            if len(user_input) == 0:
+        qline_edit = qtw.QLineEdit(self)
+        if qline_edit == 0:
                 self.textfield.setText("Please fill all fields! ")
-
 
     def main_menu(self):
         super().__init__()
         self.setWindowTitle("Car Parts Shop Main Menu")
         self.setFixedSize(900, 422)
-        window = qtw.QLabel(self)
-        window.setStyleSheet("background-color: blue")
-        window.setFixedSize(900, 422)
+        self.btn_parts_view = qtw.QPushButton('View all parts')
+        self.btn_buy_parts = qtw.QPushButton('Buy parts')
+        self.btn_log_out = qtw.QPushButton('Log out')
+        self.btn_admin_menu = qtw.QPushButton('Admin menu')
+        btn_layout = qtw.QVBoxLayout()
+        btn_layout.addWidget(self.btn_parts_view)
+        btn_layout.addWidget(self.btn_buy_parts)
+        btn_layout.addWidget(self.btn_log_out)
+        btn_layout.addWidget(self.btn_admin_menu)
+        self.setLayout(btn_layout)
+        self.show()
 
     def add_data_to_db(self):
         super().__init__()
         try:
-            user_f_name = self.user_first_name.text()
-            user_l_name = self.user_last_name.text()
-            user_email = self.user_email.text()
-            user_ph_number = self.user_phone_number.text()
-            user_password = self.user_password.text()
-            user_created = self.user_created
             mydb = mc.connect(
                 host="localhost",
                 user="root",
                 password="",
                 database="car_parts_gui")
             mycursor = mydb.cursor()
-            query = f"""
-            INSERT INTO users(first_name, last_name, email, phone_number, password, created)
-            VALUES('{user_f_name}', '{user_l_name}', '{user_email}',
-            '{user_ph_number}', '{user_password}', '{user_created}');
-            """
-            mycursor.execute(query)
+            user_f_name = self.user_first_name.text()
+            user_l_name = self.user_last_name.text()
+            user_email = self.user_email.text()
+            user_ph_number = self.user_phone_number.text()
+            user_password = self.user_password.text()
+            user_created = self.user_created
+            query = 'INSERT INTO users(first_name, last_name, email, phone_number, password, created) ' \
+                    'VALUES(%s, %s,  %s, %s, %s, %s)'
+            values = (user_f_name, user_l_name, user_email, user_ph_number, user_password, user_created)
+            mycursor.execute(query, values)
+            mydb.commit()
+            self.textfield.setText("You can log in now! ")
         except mysql.connector.Error as e:
             print(e)
 
